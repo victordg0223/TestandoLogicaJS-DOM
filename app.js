6747c7f63f10a57f9bb608f224e9fabfcb05354f
victordg0223 <victordg0223@gmail.com>
2025-10-07 01:27:58 +0000

    final 1 aula

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = "Jogo do Número secreto";
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = "Escolha um numero entre 1 e 10";
+
+function clickChute() {
+    console.log("o botão foi clicado")
+}
